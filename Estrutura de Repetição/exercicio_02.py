e466b0ddcc84eb2c5b841d0fd80dadd917c46ba7
jeffersonbezerrabr <jeffersonbezerra88@hotmail.com>
2025-12-15 22:22:54 -0300

    Exercicio 02

---
+++ Estrutura de Repetição/exercicio_02.py
@@ -0,0 +1,22 @@
+# Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual 
+# ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações.
+
+cadastros = []
+
+print("Cadastro de usuário:\n")
+
+while True:
+    login = input("Insira o nome do usuário que deseja cadastrar: ")
+    senha = input("Digite uma senha: ")
+    if senha == login:
+        print("Login e senha não podem ser iguais")
+        continue
+    cadastro = {
+        "usuario": login,
+        "senha": senha
+    }
+
+    cadastros.append(cadastro)
+    break
+
+print(cadastros)
