aa2ca973630f37b01991f51314132bf9cc3a751d
jeffersonbezerrabr <jeffersonbezerra88@hotmail.com>
2026-01-05 22:15:30 -0300

    Exercício 20

---
+++ Estrutura de Repetição/exercicio_20.py
@@ -0,0 +1,37 @@
+# Altere o programa de cálculo do fatorial, permitindo ao usuário calcular 
+# o fatorial várias vezes e limitando o fatorial a números inteiros positivos e menores que 16.
+
+while True:
+    n = input("Informe um número inteiro para ver seu fatorial: ")
+    try:
+        int_n = int(n)
+        if int_n < 0:
+            print("Precisa ser um número positivo")
+            continue
+        elif int_n > 16:
+            print("Precisa ser menor que 16")
+            continue
+        elif int_n:
+            soma = 1
+            for c in range(1, int_n + 1):
+                soma *= c
+            print(soma)
+    except ValueError:
+        print("Precisa digitar um número inteiro")
+        continue
+    
+    while True:
+        encerrar = input("Deseja encerrar? [S]im ou [N]ão: ").strip().lower()
+        
+        if encerrar.startswith("n"):
+            break  # volta para pedir outro número
+        
+        elif encerrar.startswith("s"):
+            print("Programa encerrado.")
+            exit()  # encerra o programa
+        
+        else:
+            print("Resposta inválida. Digite S ou N.")
+
+        
+            
