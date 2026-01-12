f71564672a3fd62b8425835b05b877da97429902
jeffersonbezerrabr <jeffersonbezerra88@hotmail.com>
2026-01-11 23:08:55 -0300

    Exercício 27

--- Estrutura de Repetição/exercicio_27.py
+++ Estrutura de Repetição/exercicio_27.py
@@ -11,26 +11,29 @@ while True:
             break
     except ValueError:
         print("Precisa digitar um número inteiro de turmas!")
-        
+
 c = 0
 total_alunos = 0
-while True:
-    alunos = int(input(f"Quantos alunos na turma {c + 1}: "))
-    if not alunos:
-        print("Precisa digitar algum valor")
-    elif alunos > 40:
-        print("Não pode ter mais de 40 alunos por turma!")
-    elif not int(alunos):
+
+while c < turmas:
+    try:
+        alunos = int(input(f"Quantos alunos na turma {c + 1}: "))
+
+        if alunos < 1:
+            print("Precisa digitar um valor maior que zero")
+        elif alunos > 40:
+            print("Não pode ter mais de 40 alunos por turma!")
+        else:
+            total_alunos += alunos
+            c += 1
+
+    except ValueError:
         print("Precisa digitar um número inteiro de alunos!")
-    else:
-        total_alunos += alunos
-        c += 1
-    if c == turmas:
-        break
 
 media = total_alunos / turmas
-print(f"A média de aluno é: {media}")
-    
+print(f"A média de alunos por turma é: {media:.2f}")
+
+
 
 
         
